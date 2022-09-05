b5af76a708fdc5d7bee7d272520180db86000060
aisalbek <110242974+aisalbek@users.noreply.github.com>
2022-09-05 16:27:18 +0600

    ozgordu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 51; i++) {
+            arrayList.add(i, random.nextInt(1, 101));
+
+        }
+        ArrayList<Integer> arrayjup = new ArrayList<>();
+        ArrayList<Integer> arraytak = new ArrayList<>();
+        for (Integer i : arrayList) {
+            if (i % 2 == 0) {
+                arrayjup.add(i);
+           }else {
+                arraytak.add(i);
+           }
+        }
+  System.out.println("--------------------------------Обший Сандар--------------------------------------------------------");
+  System.out.println(arrayList);
+  System.out.println("====================================================================================================");
+  System.out.println("---------------------------------Жуп Сандар-------------------------------------------------------");
+  System.out.println(arrayjup);
+  System.out.println("====================================================================================================");
+  System.out.println("-------------------------------Так Сандар---------------------------------------------------------");
+  System.out.println(arraytak);
+  System.out.println("====================================================================================================");
     }
-}
+    }
